129cdc77fb24129a6d6d48505f4843c9e037e983
Johan Wulf <johanwulf98@gmail.com>
2020-12-07 22:08:36 +0100

    minor changes

--- Sudoku/Sudoku.java
+++ Sudoku/Sudoku.java
@@ -3,7 +3,7 @@ package Sudoku;
 public class Sudoku implements SudokuSolver {
 
     private int[][] SudokuBoard;
-    
+
     public Sudoku() {
         this.SudokuBoard = new int[9][9];
     }
@@ -12,12 +12,26 @@ public class Sudoku implements SudokuSolver {
     public boolean solve() {
         for (int row = 0; row < 9; row++){
             for (int col = 0; col < 9; col++){
+                if(SudokuBoard[row][col] != 0) {
+                    if(!checkRules(row, col, SudokuBoard[row][col])) {
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return recursiveSolve();
+    }
+
+    private boolean recursiveSolve() {
+        for (int row = 0; row < 9; row++){
+            for (int col = 0; col < 9; col++){
                 if(SudokuBoard[row][col] == 0) {
                     for(int number = 1; number < 10; number++) {
                         if(checkRules(row, col, number)) {
                             SudokuBoard[row][col] = number;
 
-                            if(solve()) {
+                            if(recursiveSolve()) {
                                 return true;
                             } else {
                                 SudokuBoard[row][col] = 0;
@@ -41,6 +55,7 @@ public class Sudoku implements SudokuSolver {
         int row = x - x % 3;
         int col = y - y % 3;
 
+        // This function is not working
         for(int i = row; i < row + 3; i++) {
             for(int j = col; j < col + 3; j++) {
                 if(SudokuBoard[i][j] == number) {
